a0ca0fd692fe4f6fd3eff0b66fc8caabfb0cabc8
Hasheem Reddick <hasznoneother@gmail.com>
2022-10-20 12:34:49 -0400

    added variables

--- app.js
+++ app.js
@@ -3,8 +3,30 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+const firstName = "Johnson";
 
-// Exercise 2
+let lastName = "Reddick";
 
-// Exercise 3
+var age= 47;
+
+let firstName = "Hasheem";
+let lastName = "Reddick";
+let age = 47;
+
+let language = "Javascript";
+let createdYear = 1995;
+
+let isCaseSensitive= true;
+
+let price = 19.99;
+let isOnsale = false
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 772;
+let bookmark = 456;
+let hasRead = true;
